41119ba0912072753a8eaba3a401ebaae205031e
bakhtiarmuhib <muhibanikcode@gmail.com>
2019-04-01 17:06:50 +0600

    right side triangle

---
+++ right side triangle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+for i in range(1,5):
+    for j in range(1,5):
+    	if (i+j>4):
+    		print "*",
+    	else:
+    		print " ",
+    print
